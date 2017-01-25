c949e3e58927e4a38f143fbe113d8d2a60a6eff0
Mofaggol Hoshen <summoncse@gmail.com>
2017-01-25 16:29:00 +0100

    Refactored

---
+++ AnomalyDetection.Interfaces/Settings/CheckingSampleSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnomalyDetection.Interfaces
+{
+    /// <summary>
+    /// The desired settings by the user for checking to which cluster a sample belongs
+    /// </summary>
+    public class CheckingSampleSettings
+    {
+        /// <summary>
+        /// Settings to the clustering intance that contains the clusters data
+        /// </summary>
+        public SaveLoadSettings LoadProjectSettings { get; internal set; }
+
+        /// <summary>
+        /// The sample to be checked
+        /// </summary>
+        public double[] Sample { get; internal set; }
+
+        /// <summary>
+        /// A value in % representing the tolerance to possible outliers
+        /// </summary>
+        public double Tolerance { get; internal set; }
+
+        /// <summary>
+        /// Constructor to create the desired settings by the user for checking to which cluster a sample belongs
+        /// </summary>
+        /// <param name="LoadProjectSettings">Settings to the clustering intance that contains the clusters data</param>
+        /// <param name="Sample">The sample to be checked</param>
+        /// <param name="tolerance">A value in % representing the tolerance to possible outliers</param>
+        public CheckingSampleSettings(SaveLoadSettings LoadProjectSettings, double[] Sample, double tolerance = 0)
+        {
+            this.LoadProjectSettings = LoadProjectSettings;
+            this.Sample = Sample;
+            this.Tolerance = tolerance;
+        }
+    }
+}
